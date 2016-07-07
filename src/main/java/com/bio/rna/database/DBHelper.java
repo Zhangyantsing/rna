7b633da9b81d4b2fe61a9fc1b0f9a8c5c8a2bcdd
Zhangyantsing <zhangyantsing@163.com>
2016-07-07 18:39:11 +0800

    reinnore

--- src/main/java/com/bio/rna/database/DBHelper.java
+++ src/main/java/com/bio/rna/database/DBHelper.java
@@ -1,5 +1,35 @@
 package com.bio.rna.database;
 
-public class DBHelper {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+/**
+ * Mysql DBHelper
+ * 
+ * @author zhangyq
+ *
+ */
+public class DBHelper {
+	private static String driver_class = "com.mysql.jdbc.Driver";
+	private static String user = "zhangyq";
+	private static String password = "Pi31415938";
+	
+	private String db_name = null;
+	private String driver_url = "jdbc:mysql://101.6.97.186:3306/" + db_name + "?useSSL=false";
+	
+	private static Connection conn = null;
+	private PreparedStatement pst = null;
+	private ResultSet rst = null;
+	
+	/*
+	 * Connection
+	 */
+	public DBHelper(){
+		try{
+			conn = DBHelper.getInstance();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
