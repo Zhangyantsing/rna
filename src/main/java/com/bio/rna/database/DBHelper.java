2701260ba39c1e2451e4ec2979059b40b17d265e
zhangyq <zhangyantsing@163.com>
2016-07-08 04:08:08 +0800

    A

--- src/main/java/com/bio/rna/database/DBHelper.java
+++ src/main/java/com/bio/rna/database/DBHelper.java
@@ -1,8 +1,11 @@
 package com.bio.rna.database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Mysql DBHelper
@@ -11,14 +14,13 @@ import java.sql.ResultSet;
  *
  */
 public class DBHelper {
-	private static String driver_class = "com.mysql.jdbc.Driver";
 	private static String user = "zhangyq";
 	private static String password = "Pi31415938";
 	
 	private String db_name = null;
 	private String driver_url = "jdbc:mysql://101.6.97.186:3306/" + db_name + "?useSSL=false";
 	
-	private static Connection conn = null;
+	private Connection conn = null;
 	private PreparedStatement pst = null;
 	private ResultSet rst = null;
 	
@@ -27,9 +29,106 @@ public class DBHelper {
 	 */
 	public DBHelper(){
 		try{
-			conn = DBHelper.getInstance();
+			conn = DBHelper.getConnInstance();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
+	/**
+     * 单例模式
+     * 线程同步
+     * @return
+     */
+    private synchronized Connection getConnInstance() {
+        if(conn == null){
+            try {
+
+                conn = DriverManager.getConnection(driver_url, user, password);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            System.out.println("连接数据库成功");
+        }
+        return conn;
+    }
+
+
+    /**
+     * close
+     */
+    public void close() {
+
+        try {
+            if (conn != null) {
+                DBHelper.conn.close();
+            }
+            if (pst != null) {
+                this.pst.close();
+            }
+            if (rst != null) {
+                this.rst.close();
+            }
+            System.out.println("关闭数据库成功");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * query
+     * 
+     * @param sql
+     * @param sqlValues
+     * @return ResultSet
+     */
+    public ResultSet executeQuery(String sql, List<String> sqlValues) {
+        try {
+            pst = conn.prepareStatement(sql);
+            if (sqlValues != null && sqlValues.size() > 0) {
+                setSqlValues(pst, sqlValues);
+            }
+            rst = pst.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rst;
+    }
+
+    /**
+     * update
+     * 
+     * @param sql
+     * @param sqlValues
+     * @return result
+     */
+    public int executeUpdate(String sql, List<String> sqlValues) {
+        int result = -1;
+        try {
+            pst = conn.prepareStatement(sql);
+            if (sqlValues != null && sqlValues.size() > 0) {
+                setSqlValues(pst, sqlValues);
+            }
+            result = pst.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    /**
+     * sql set value
+     * 
+     * @param pst
+     * @param sqlValues
+     */
+    private void setSqlValues(PreparedStatement pst, List<String> sqlValues) {
+        for (int i = 0; i < sqlValues.size(); i++) {
+            try {
+                pst.setObject(i + 1, sqlValues.get(i));
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
