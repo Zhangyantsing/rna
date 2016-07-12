8bf67b55c32727ef2a0c0d27e8064dd26ec794fe
Zhangyantsing <zhangyantsing@163.com>
2016-07-12 20:56:09 +0800

    annotation finish

--- src/main/java/com/bio/rna/App.java
+++ src/main/java/com/bio/rna/App.java
@@ -2,7 +2,13 @@ package com.bio.rna;
 
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.naming.directory.SearchControls;
+import javax.print.attribute.standard.PrinterLocation;
 
 import org.omg.IOP.RMICustomMaxStreamFormat;
 
@@ -19,30 +25,8 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-    	String file_name = "D:\\\\Java\\\\WorkSpace\\\\Bio\\\\rna\\\\data\\\\hg38.basic.annotation.tb";
     	DBHelper dh = new DBHelper("rnamod_annotation");
-    	TBHelper.createAnnTB(dh, "hg38");
-    	List<String> list = new ArrayList<String>();
-
-		list.add("chr");
-		list.add("strand");
-		list.add("start");
-		list.add("end");
-		list.add("shortType");
-		list.add("description");
-		list.add("type");
-		list.add("gene");
-		list.add("gene_des");
-		list.add("gene_rank");
-		list.add("gene_total");
-    	TBHelper.FileIntoTB(dh, file_name, "hg38", list);
-/*    	ResultSet rs = dh.executeQuery("test", "SELECT * FROM testann", null);
-
-    	while(rs.next()){
-    		String s = rs.getString("id");
-    		System.out.println(s);
-    	}
-    */
-    	new Annotation(dh,"hg38").show();
+    	Annotation anno = new Annotation(dh, "hg38");
+    	System.out.println(anno.anno("chr20", 10000000));
     }
 }
--- src/main/java/com/bio/rna/query/Annotation.java
+++ src/main/java/com/bio/rna/query/Annotation.java
@@ -2,32 +2,57 @@ package com.bio.rna.query;
 
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.bio.rna.database.DBHelper;
 
 public class Annotation {
-	private List<Integer> sList = new ArrayList<Integer>();
+	private Map<String, List<Integer>> chrList = new HashMap<String, List<Integer>>();
+
+		
+//binary search		
+	public int search(List<Integer> number, int des) {
+	    	if(des < 1) return -1;
+	        int low = 0;
+	        int upper = number.size() - 1;
+	        while (low <= upper) {
+	            int mid = (low + upper) / 2;  
+	            if(number.get(mid) > des){
+	            	upper = mid - 1;
+	            }
+	            else if(number.get(mid) < des){
+	            	low = mid + 1;
+	            }
+	            else {
+					return mid;
+				}
+	        }  
+	        return upper;
+	    }
+
+//annotation function	    
+	public int anno(String chr, int location){
+		return chrList.get(chr).get(0) + search(chrList.get(chr), location) - 1;
+	}
+
 	public Annotation(){}
 	public Annotation(DBHelper dh, String tb_name) throws Exception{
-    	ResultSet rs = dh.executeQuery("test", "SELECT * FROM hg38", null);
+    	ResultSet rs = dh.executeQuery("anno", "SELECT id,chr,start FROM hg38", null);
+    	String chr = null;
+    	int start = 0;
+    	
     	while(rs.next()){
-    		sList.add(Integer.parseInt(rs.getString("start")));
+    		chr = rs.getString("chr");
+    		start = rs.getInt("start");
+    		if(!chrList.containsKey(chr)){
+    			chrList.put(rs.getString("chr"), new ArrayList<>());
+    			chrList.get(chr).add(rs.getInt("id"));
+    		}
+    		chrList.get(chr).add(start);
     	}
+    	dh.close("anno");
 	}
-	public void show(){
-		for(int i: sList){
-			System.out.println(i);
-		}
-	}
-	public static int anno(int location) throws Exception{
-		int i=0;
-		DBHelper dh = new DBHelper("rnamod_annotation");
-    	ResultSet rs = dh.executeQuery("test", "SELECT * FROM testann", null);
-    	while(rs.next()){
-    		String s = rs.getString("id");
-    		System.out.println(s);
-    	}
-		return i;
-	}
+
 }
